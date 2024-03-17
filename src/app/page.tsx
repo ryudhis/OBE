56f5e242b417864d00e128db0c5f70d535714917
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2024-03-18 00:10:03 +0700

    fix data table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 
 import React from "react";
-import { Button } from "@/components/ui/button"
-import { useToast } from "@/components/ui/use-toast"
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 
 const Page = () => {
-  const { toast } = useToast()
+  const { toast } = useToast();
   return (
     <Button
       variant="outline"
       onClick={() => {
         toast({
-          title: "Uh oh! Something went wrong.",
           description: "There was a problem with your request.",
-        })
+        });
       }}
     >
       Show Toast
