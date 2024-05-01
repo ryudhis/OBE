d76d898ce1ec34b1496b5f4607f5d78a6d5b67f4
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2024-05-02 00:41:52 +0700

    fix error deploy reff page

--- src/app/dashboard/data/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { useRouter } from "next/navigation";
-
-const Data = () => {
-  return useRouter().push("/dashboard/data/pl");
-};
-
-export default Data;
