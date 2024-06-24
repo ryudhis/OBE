026e7673e1b43109603f8299bf31461f754a2472
Juan <juantanuwijaya@gmail.com>
2024-06-25 00:01:05 +0700

    fix end point

--- src/app/api/inputNilai/route.js
+++ src/app/api/inputNilai/route.js
@@ -1,6 +1,6 @@
 import prisma from "@/utils/prisma";
 
-export async function GET(req, res) {
+export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const prodi = searchParams.get("prodi") || ""; // Access prodi query parameter
 
