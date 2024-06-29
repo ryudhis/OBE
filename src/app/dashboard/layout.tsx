fe2851404e2948414e71aecc5cfcf1d41786f9a0
Ryudhis <ryudhis.id@gmail.com>
2024-06-29 15:36:25 +0700

    add rencana asesment dan evaluasi

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header />
-      <div>{children}</div>
+      <div className="bg-[#FAFAFA]">{children}</div>
     </div>
   );
 };
