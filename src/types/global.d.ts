3c3ae64e6edea87a17d01ab92cd0ab2ceff3153c
Ryudhis <ryudhis.id@gmail.com>
2025-02-27 14:27:11 +0700

    feat(feature): mpdify mahasiswa type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -325,6 +325,8 @@ declare global {
     prodi: Prodi;
     prodiId: string;
     performaMahasiswa: performaMahasiswa[];
+    mahasiswa_CPMK: Mahasiswa_CPMK[];
+    mahasiswa_MK_CPMK: Mahasiswa_MK_CPMK[];
   }
 
   export interface InputNilai {
